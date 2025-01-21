f45b430388492593035ca25ae889f5a8e1cc7ae1
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2025-01-21 11:45:32 +0100

    feat: enhance about page
    Co-authored-by: Katasc22 <k.aschauer@student.tugraz.at>

--- pages/about.vue
+++ pages/about.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="info" class="content-container">
-    <h2>{{$t("about.title")}}</h2>
-    
-    <p>{{$t("about.addText1")}} {{$t("about.addText2")}}</p>
-    <p>{{$t("about.addText3")}}</p>
-
-    <div class="text-center">
+    <h2>{{ $t("about.title") }}</h2>
+    <p>{{ $t("about.addText1") }} {{ $t("about.addText2") }}</p>
+    <p>{{ $t("about.addText3") }}</p>
+    <p>{{ $t("about.linktoIndiYoung") }}</p>
+    <a class="btn-primary" target="_blank" href="https://indiyoung.com/">{{
+        $t("about.buttonIndiYoung")
+      }}</a>
+    <div class="text-center mt-20">
+      <h4>{{ $t("about.attributions") }}</h4>
       <p class="text-[16px] m-0">
-        {{$t("about.attributions")}}
-        <a href="https://www.flaticon.com/free-icons/architecture-and-city" title="architecture and city icons">
-          {{$t("about.iconsBy")}}
+        <a href="https://www.flaticon.com/free-icons/architecture-and-city"
+           title="architecture and city icons">
+          {{ $t("about.iconsBy") }}
         </a>
       </p>
       <p class="text-[16px]">
-        {{$t("about.copyright")}}
+        {{ $t("about.copyright") }}
       </p>
     </div>
   </div>
@@ -22,13 +25,14 @@
 <style scoped>
 html,
 body {
-margin: 0;
-padding: 0;
-height: 100%;
+  margin: 0;
+  padding: 0;
+  height: 100%;
 }
+
 .content-container {
-  width: 100%; 
-  margin: 0 auto; 
-  overflow-x: hidden; 
+  width: 100%;
+  margin: 0 auto;
+  overflow-x: hidden;
 }
 </style>
