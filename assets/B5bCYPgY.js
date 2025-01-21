ff45b39d247648c9bf06317dd5b43be934b809da
Katasc22 <k.aschauer@student.tugraz.at>
2025-01-21 10:40:34 +0100

    Updates

--- assets/B5bCYPgY.js
+++
@@ -1,1 +0,0 @@
-import{N as t,E as a,c as o,o as r}from"./CN0Rz_B-.js";const u={__name:"index",setup(c){const e=t();return a(()=>{e.replace({path:"/diagram"})}),(n,s)=>(r(),o("div"))}};export{u as default};
