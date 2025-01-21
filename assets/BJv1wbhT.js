80b69088e922f6f202efd7bbffb2c1e5c7034a35
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2025-01-21 12:48:23 +0100

    Updates

---
+++ assets/BJv1wbhT.js
@@ -0,0 +1,1 @@
+import{N as t,E as a,c as o,o as r}from"./Dc9VOdb1.js";const u={__name:"index",setup(c){const e=t();return a(()=>{e.replace({path:"/diagram"})}),(n,s)=>(r(),o("div"))}};export{u as default};
