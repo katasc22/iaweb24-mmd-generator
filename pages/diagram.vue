f303606f82d31fc105b9a7271e8aa9cc9b4724aa
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2025-01-21 11:29:15 +0100

    feat: adaptive layout and sizes

--- pages/diagram.vue
+++ pages/diagram.vue
@@ -1,21 +1,15 @@
-<style>
-.diagram svg {
-  height: 100%;
-}
-</style>
-
 <template>
   <div id="diagram" class="content-container">
     <div id="buttonbar" class="w-full flex flex-row justify-center mb-8">
       <!-- File Upload Input -->
       <!-- <label>{{$t("common.fileUpload")}}</label> -->
       <input
-        type="file"
-        ref="fileInput"
-        @change="onFileChange"
-        accept=".xlsx"
-        class="hidden"
-        style="display: none;"
+          type="file"
+          ref="fileInput"
+          @change="onFileChange"
+          accept=".xlsx"
+          class="hidden"
+          style="display: none;"
       />
 
       <!-- Styled Button -->
@@ -29,12 +23,12 @@
         {{$t("common.downloadExcel")}}
       </button>
 
-        <button
-            class="btn-primary"
-            @click="openModal"
-        >
-          {{$t("navbar.menu.settings") }}
-        </button>
+      <button
+          class="btn-primary"
+          @click="openModal"
+      >
+        {{$t("navbar.menu.settings") }}
+      </button>
       <TransitionRoot appear :show="isOpen" as="template">
         <Dialog @close="closeModal" class="relative z-10">
           <TransitionChild
@@ -75,102 +69,89 @@
           </div>
         </Dialog>
       </TransitionRoot>
-
     </div>
-    <div id="panelcontainer" class="flex flex-row flex-grow w-full max-h-[80vh] bg-white"> <!--max-w-screen-lg-->
-        <div :style="{ flex: leftPanelFlex }" class="overflow-auto p-4">
-          <h3>{{$t("common.fileContent")}}</h3>
-          <div v-if="gridData.length" class="h-full w-full">
-              <canvas-datagrid
-                  :data="gridData"
-                  :editable="true"
-                  @data-changed="handleDataChange"
-              ></canvas-datagrid>
+
+    <div id="panelcontainer" class="flex flex-row flex-grow w-full max-h-[80vh] bg-white rounded-2xl"> <!--max-w-screen-lg-->
+      <div :style="{ flex: leftPanelFlex }" class="p-4 flex flex-col">
+        <h3>{{$t("common.fileContent")}}</h3>
+        <div v-if="gridData.length" class="flex-auto relative">
+          <div class="h-full w-full absolute overflow-scroll">
+            <canvas-datagrid
+                :data="gridData"
+                :editable="true"
+                @data-changed="handleDataChange"
+            />
           </div>
-          <p v-else class="text-gray-500">{{$t("common.noContent")}}</p>
         </div>
+        <p v-else class="text-gray-500">{{$t("common.noContent")}}</p>
+      </div>
       <div
           id="resizeHandle"
           :style="{ flex: centralPanelFlex, backgroundColor: '#d1d5db' }"
           class="cursor-col-resize flex items-center justify-center"
           @mousedown="startResizing"
       >
-          <p class="text-center text-sm font-medium text-gray-500">||</p>
+        <p class="text-center text-sm font-medium text-gray-500">||</p>
       </div>
-        <div :style="{ flex: rightPanelFlex }" class="p-4">
-          <h3 style="height: 5%;">{{$t("common.svgView")}}</h3>
-          <div v-if="svg" style="width: 100%; height: 95%;">
-            <div
-              ref="svgContainer"
-              style="width: 100%; height: 100%;"
-              class="relative overflow-hidden"
-              @wheel.prevent="zoomSvg"
-              @mousedown="startPan"
-              @mousemove="panSvg"
-              @mouseup="endPan"
-              @mouseleave="endPan">
-              <div
-                ref="svgElement"
-                v-html="svg"
-                class="absolute top-100 left-100   transform origin-top-left"
-                :style="svgTransform"
-              ></div>
-            </div>
-        <div
-            id="resizeHandle"
-            class="w-2 cursor-col-resize"
-            @mousedown="startResizing"
-        >
+      <div :style="{ flex: rightPanelFlex }" class="p-4 flex flex-col">
+        <h3>{{$t("common.svgView")}}</h3>
+        <div v-if="svg"
+             class="h-full flex-auto relative overflow-hidden rounded-2xl"
+             @wheel.prevent="zoomSvg"
+             @mousedown="startPan"
+             @mousemove="panSvg"
+             @mouseup="endPan"
+             @mouseleave="endPan">
+          <div
+              v-html="svg"
+              class="diagram h-full absolute"
+              :style="svgTransform"
+          />
         </div>
-        <div :style="{ flex: rightPanelFlex }" class="overflow-auto p-4 flex flex-col">
-          <h3>{{$t("common.svgView")}}</h3>
-          <div v-if="svg" class="diagram h-full flex-auto mt-6 p-4 bg-white rounded shadow overflow-auto"
-               v-html="svg">
-          </div>
-          <p v-else class="text-gray-500">{{$t("common.svgUnavailable")}}</p>
-        </div>
+        <p v-else class="text-gray-500">{{$t("common.noContent")}}</p>
+      </div>
     </div>
     <p>Blocks: {{blockCount}}, Towers: {{towerCount}}</p>
   </div>
 </template>
 
 <script setup>
-  import {generateMentalModelDiagram} from "~/diagrams/mental-model-diagram";
-  import {useDataStore} from "~/stores/dataStore";
-  import {onMounted} from "vue";
-  import { handleFileUpload } from "~/diagrams/fileUtils";
-  import { utils, writeFile } from "xlsx";
-  import {
-    TransitionRoot,
-    TransitionChild,
-    Dialog,
-    DialogPanel,
-  } from '@headlessui/vue'
+import {generateMentalModelDiagram} from "~/diagrams/mental-model-diagram";
+import {useDataStore} from "~/stores/dataStore";
+import {onMounted} from "vue";
+import { handleFileUpload } from "~/diagrams/fileUtils";
+import { utils, writeFile } from "xlsx";
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+} from '@headlessui/vue'
 
-  const dataStore = useDataStore();
+const dataStore = useDataStore();
 
-  // Access shared data from the Pinia store
-  const gridData = ref(dataStore.gridData);
-  const diagramData = ref(dataStore.diagramData);
-  const svg = ref(dataStore.svg);
-  const blockCount = ref(dataStore.blockCount);
-  const towerCount = ref(dataStore.towerCount);
-  const fileName = ref(dataStore.fileName);
-  const fileInput = ref(null);
-  //settings popup:
-  const isOpen = ref(false)
+// Access shared data from the Pinia store
+const gridData = ref(dataStore.gridData);
+const diagramData = ref(dataStore.diagramData);
+const svg = ref(dataStore.svg);
+const blockCount = ref(dataStore.blockCount);
+const towerCount = ref(dataStore.towerCount);
+const fileName = ref(dataStore.fileName);
+const fileInput = ref(null);
+//settings popup:
+const isOpen = ref(false)
 
-  // Panel resizing state
-  let isResizing = false;
-  let startX = 0;
-  let leftPanelFlex = ref(0.495);
-  let rightPanelFlex = ref(0.495);
-  let centralPanelFlex = ref(0.01);
+// Panel resizing state
+let isResizing = false;
+let startX = 0;
+let leftPanelFlex = ref(0.495);
+let rightPanelFlex = ref(0.495);
+let centralPanelFlex = ref(0.01);
 
-  const svgScale = ref(1);
-  const svgOffset = ref({ x: 0,y:0 });
-  const isPanning = ref(false);
-  const panStart = ref({ x: 0, y:0 });
+const svgScale = ref(1);
+const svgOffset = ref({ x: 0,y:0 });
+const isPanning = ref(false);
+const panStart = ref({ x: 0, y:0 });
 const svgTransform = computed(() => {
   const transform = `scale(${svgScale.value}) translate(${svgOffset.value.x}px, ${svgOffset.value.y}px)`;
   console.log("SVG Transform:", transform);
@@ -180,15 +161,15 @@ const svgTransform = computed(() => {
   };
 });
 
-  function closeModal() {
-    isOpen.value = false
-    svg.value = generateMentalModelDiagram(diagramData.value, dataStore.settings);
-  }
-  function openModal() {
-    isOpen.value = true
-  }
+function closeModal() {
+  isOpen.value = false
+  svg.value = generateMentalModelDiagram(diagramData.value, dataStore.settings);
+}
+function openModal() {
+  isOpen.value = true
+}
 
-  async function onFileChange(event) {
+async function onFileChange(event) {
   try {
     const sheetData = await handleFileUpload(event);
     fileName.value = event.target.files[0].name.split(".")[0]; // Extract the file name
@@ -206,79 +187,79 @@ const svgTransform = computed(() => {
 }
 
 
-  onMounted(() => {
-    if(gridData.value.length) {
-      generateDiagram(gridData.value);
+onMounted(() => {
+  if(gridData.value.length) {
+    generateDiagram(gridData.value);
+  }
+})
+
+// Watch for changes in gridData and regenerate the SVG
+watch(
+    gridData,
+    (newData) => {
+      generateDiagram(newData);
+    },
+    { deep: true }
+);
+
+function triggerFileInput() {
+  if (fileInput.value) {
+    fileInput.value.click(); // Programmatically click the hidden input
+  }
+}
+
+// Handle user edits to the grid data
+function handleDataChange(newData) {
+  gridData.value = newData; // Update the local and store state
+  dataStore.updateGridData(newData);
+}
+
+// Methods for resizing panels
+function startResizing(event) {
+  isResizing = true;
+  startX = event.clientX;
+  document.addEventListener("mousemove", resizePanels);
+  document.addEventListener("mouseup", stopResizing);
+}
+
+function resizePanels(event) {
+  if (isResizing) {
+    const deltaX = event.clientX - startX;
+    const containerWidth = document.getElementById("panelcontainer").offsetWidth;
+    const totalFlex = leftPanelFlex.value + rightPanelFlex.value;
+    const newLeftFlex = leftPanelFlex.value + deltaX / containerWidth;
+    const newRightFlex = totalFlex - newLeftFlex;
+    if (newLeftFlex >= 0.1 && newRightFlex >= 0.1) {
+      leftPanelFlex.value = newLeftFlex;
+      rightPanelFlex.value = newRightFlex;
+      startX = event.clientX;
     }
-  })
+  }
+}
 
-  // Watch for changes in gridData and regenerate the SVG
-  watch(
-      gridData,
-      (newData) => {
-        generateDiagram(newData);
-      },
-      { deep: true }
-  );
+function stopResizing() {
+  isResizing = false;
+  document.removeEventListener("mousemove", resizePanels);
+  document.removeEventListener("mouseup", stopResizing);
+}
 
-  function triggerFileInput() {
-    if (fileInput.value) {
-      fileInput.value.click(); // Programmatically click the hidden input
-    }
+function downloadSvg() {
+  if (!diagramData.value) {
+    alert("No SVG content to download.");
+    return;
   }
 
-  // Handle user edits to the grid data
-  function handleDataChange(newData) {
-    gridData.value = newData; // Update the local and store state
-    dataStore.updateGridData(newData);
-  }
+  const svg = generateMentalModelDiagram(diagramData.value, dataStore.settings);
+  const blob = new Blob([svg], { type: "image/svg+xml" });
+  const link = document.createElement("a");
+  const url = URL.createObjectURL(blob);
+  link.href = url;
+  link.download = `${fileName.value}.svg`;
+  link.click();
+  URL.revokeObjectURL(url);
+}
 
-  // Methods for resizing panels
-  function startResizing(event) {
-    isResizing = true;
-    startX = event.clientX;
-    document.addEventListener("mousemove", resizePanels);
-    document.addEventListener("mouseup", stopResizing);
-  }
-
- function resizePanels(event) {
-    if (isResizing) {
-      const deltaX = event.clientX - startX;
-      const containerWidth = document.getElementById("panelcontainer").offsetWidth;
-      const totalFlex = leftPanelFlex.value + rightPanelFlex.value;
-      const newLeftFlex = leftPanelFlex.value + deltaX / containerWidth;
-      const newRightFlex = totalFlex - newLeftFlex;
-      if (newLeftFlex >= 0.1 && newRightFlex >= 0.1) {
-        leftPanelFlex.value = newLeftFlex;
-        rightPanelFlex.value = newRightFlex;
-        startX = event.clientX;
-      }
-    }
-  }
-
-  function stopResizing() {
-    isResizing = false;
-    document.removeEventListener("mousemove", resizePanels);
-    document.removeEventListener("mouseup", stopResizing);
-  }
-
-  function downloadSvg() {
-    if (!diagramData.value) {
-      alert("No SVG content to download.");
-      return;
-    }
-
-    const svg = generateMentalModelDiagram(diagramData.value, dataStore.settings);
-    const blob = new Blob([svg], { type: "image/svg+xml" });
-    const link = document.createElement("a");
-    const url = URL.createObjectURL(blob);
-    link.href = url;
-    link.download = `${fileName.value}.svg`;
-    link.click();
-    URL.revokeObjectURL(url);
-  }
-
-  const downloadExcel = () => {
+const downloadExcel = () => {
   if (!gridData.value.length) {
     alert("No data to download.");
     return;
@@ -291,66 +272,71 @@ const svgTransform = computed(() => {
   writeFile(workbook, `${fileName.value || "updated-diagram-data"}.xlsx`);
 };
 
-  function generateDiagram(jsonData) {
-    let newBlockCount = 0;
-    let newTowerCount = 0;
-    let block = null;
-    let tower = null;
-    const data = {};
-    jsonData
-        .slice(1)
-        .forEach((row) => {
-          if (row[0]?.trim()) {
-            block = row[0];
-            data[block] = {};
-            newBlockCount++;
-          }
-          if (row[1]?.trim()) {
-            tower = row[1];
-            data[block][tower] = [];
-            newTowerCount++;
-          }
-          if (row[2]?.trim()) {
-            data[block][tower].push(row[2]);
-          }
-        });
-    // Generate the SVG and update the state
-    diagramData.value = data;
-    const opts = dataStore.settings
-    svg.value = generateMentalModelDiagram(data, opts);
-    blockCount.value = newBlockCount;
-    towerCount.value = newTowerCount;
-
-    dataStore.updateDiagramData(diagramData.value, svg.value, newBlockCount, newTowerCount);
-  }
-
-  function zoomSvg(event) {
-    const zoomFactor = 1.1;
-    const rect = event.currentTarget.getBoundingClientRect();
-    const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top;
-    const zoomDirection = event.deltaY < 0 ? zoomFactor : 1 / zoomFactor;
-    svgScale.value *= zoomDirection;
-    svgOffset.value.x -= (mouseX / svgScale.value) * (zoomDirection - 1);
-    svgOffset.value.y -= (mouseY / svgScale.value) * (zoomDirection - 1);
-  }
-  function startPan(event) {
-    isPanning.value = true;
-    panStart.value = { x: event.clientX - svgOffset.value.x, y: event.clientY - svgOffset.value.y };
-  }
-  function panSvg(event) {
-    if (isPanning.value) {
-      svgOffset.value = { x: event.clientX - panStart.value.x, y: event.clientY - panStart.value.y };
+function generateDiagram(jsonData) {
+  let newBlockCount = 0;
+  let newTowerCount = 0;
+  let block = null;
+  let tower = null;
+  const data = {};
+  jsonData
+  .slice(1)
+  .forEach((row) => {
+    if (row[0]?.trim()) {
+      block = row[0];
+      data[block] = {};
+      newBlockCount++;
     }
-  }
-  function endPan() {
-    isPanning.value = false;
-  }
+    if (row[1]?.trim()) {
+      tower = row[1];
+      data[block][tower] = [];
+      newTowerCount++;
+    }
+    if (row[2]?.trim()) {
+      data[block][tower].push(row[2]);
+    }
+  });
+  // Generate the SVG and update the state
+  diagramData.value = data;
+  const opts = dataStore.settings
+  svg.value = generateMentalModelDiagram(data, opts);
+  blockCount.value = newBlockCount;
+  towerCount.value = newTowerCount;
 
+  dataStore.updateDiagramData(diagramData.value, svg.value, newBlockCount, newTowerCount);
+}
+
+function zoomSvg(event) {
+  const zoomFactor = 1.1;
+  const rect = event.currentTarget.getBoundingClientRect();
+  const mouseX = event.clientX - rect.left;
+  const mouseY = event.clientY - rect.top;
+  const zoomDirection = event.deltaY < 0 ? zoomFactor : 1 / zoomFactor;
+  svgScale.value *= zoomDirection;
+  svgOffset.value.x -= (mouseX / svgScale.value) * (zoomDirection - 1);
+  svgOffset.value.y -= (mouseY / svgScale.value) * (zoomDirection - 1);
+}
+function startPan(event) {
+  isPanning.value = true;
+  panStart.value = { x: event.clientX - svgOffset.value.x, y: event.clientY - svgOffset.value.y };
+}
+function panSvg(event) {
+  if (isPanning.value) {
+    svgOffset.value = {x: event.clientX - panStart.value.x, y: event.clientY - panStart.value.y};
+  }
+}
+
+function endPan() {
+  isPanning.value = false;
+}
 </script>
 
-<style scoped>
+<style>
+.diagram svg {
+  height: 100%;
+}
+</style>
 
+<style scoped>
 .content-container {
   width: 100%; /* Assure que la largeur correspond au parent */
   margin: 0 auto; /* Centre horizontalement */
