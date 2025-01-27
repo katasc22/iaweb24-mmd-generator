b06d7b144e01a801de48349b886d3b29e715eb81
Katasc22 <k.aschauer@student.tugraz.at>
2025-01-27 10:15:48 +0100

    Updates

---
+++ assets/BFZ2oetg.js
@@ -0,0 +1,1 @@
+import{O as t,q as a,c as o,o as r}from"./DFOnnopK.js";const u={__name:"index",setup(c){const e=t();return a(()=>{e.replace({path:"/diagram"})}),(n,s)=>(r(),o("div"))}};export{u as default};
--- assets/C2xPubyy.js
+++
@@ -1,1 +0,0 @@
-import{O as t,q as a,c as o,o as r}from"./BcWGsbt-.js";const u={__name:"index",setup(c){const e=t();return a(()=>{e.replace({path:"/diagram"})}),(n,s)=>(r(),o("div"))}};export{u as default};
