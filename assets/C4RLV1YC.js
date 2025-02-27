d6aa8b816a2d79164486b65f9ff58f90cc1c3f0e
Katasc22 <k.aschauer@student.tugraz.at>
2025-02-27 13:34:50 +0100

    Updates

---
+++ assets/C4RLV1YC.js
@@ -0,0 +1,1 @@
+import{q as t,O as a,c as o,o as r}from"./uAHWZinJ.js";const u={__name:"index",setup(c){const e=a();return t(()=>{e.replace({path:"/diagram"})}),(n,s)=>(r(),o("div"))}};export{u as default};
