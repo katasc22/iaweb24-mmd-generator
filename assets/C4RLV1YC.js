fa76d7b03fba1a2504ce8769c432ed7cb70e5cc1
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2025-02-28 12:03:32 +0100

    Updates

--- assets/C4RLV1YC.js
+++
@@ -1,1 +0,0 @@
-import{q as t,O as a,c as o,o as r}from"./uAHWZinJ.js";const u={__name:"index",setup(c){const e=a();return t(()=>{e.replace({path:"/diagram"})}),(n,s)=>(r(),o("div"))}};export{u as default};
